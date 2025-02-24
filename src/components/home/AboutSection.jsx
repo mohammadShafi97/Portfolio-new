351f99380cb136cfc8be104b7a4aafa4e82b8ff8
clawlynx <clawlynx77@gmail.com>
2025-02-24 18:17:08 +0530

    added about section

---
+++ src/components/home/AboutSection.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function AboutSection() {
+  return (
+    <div className="px-[120px] py-5 flex flex-col gap-5">
+      <h3 className="text-3xl font-semibold ">About Me</h3>
+      <div className="flex flex-col gap-5 max-w-[600px] text-lg">
+        <p>
+          Hi, I'm Mohammad Shafi S., a passionate Full Stack Web Developer and
+          Engineer from Kollam, Kerala, India. I graduated from TKM College of
+          Engineering, one of the most prestigious institutions in the state.
+        </p>
+        <p>My Journey in Tech</p>
+        <p>
+          From childhood, I have been fascinated by computers, which led me to
+          develop strong technical expertise. My passion for coding is so deep
+          that I can't end my day without spending at least two hours coding and
+          learning about new technologies.
+        </p>
+        <p>Skills & Expertise</p>
+        <p>
+          I specialize in the MERN stack and have a strong foundation in: HTML,
+          CSS, JavaScript, Bootstrap, React, Node.js, Express.js, MongoDB, Git,
+          GitHub
+        </p>
+        <p>
+          Beyond development, I also worked as a Technical Coordinator in a
+          company for two years, where I honed my problem-solving and leadership
+          skills.
+        </p>
+        <p>Teaching & Mentorship</p>
+        <p>
+          I'm also a part-time teacher, and over 100 students have attended my
+          coaching for higher secondary education. I love sharing knowledge and
+          guiding aspiring developers and students toward success.
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LandingSection from "../../components/home/LandingSection";
+import AboutSection from "../../components/home/AboutSection";
 
 export default function HomePage() {
   return (
     <div>
       <LandingSection />
+      <AboutSection />
     </div>
   );
 }
